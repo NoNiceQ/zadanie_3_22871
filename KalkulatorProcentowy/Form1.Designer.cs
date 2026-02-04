51e3ce5c7cf32fbc5b22b2883435370a55c9f2e4
NoNiceQ <148637238+NoNiceQ@users.noreply.github.com>
2026-02-04 15:04:43 +0100

    aktualny stan
    Co-authored-by: chomiuk22871 <chomiuk22871@stud.akademiabialska.pl>

--- KalkulatorProcentowy/Form1.Designer.cs
+++ KalkulatorProcentowy/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_test
+            // 
+            this.button_test.Location = new System.Drawing.Point(282, 182);
+            this.button_test.Name = "button_test";
+            this.button_test.Size = new System.Drawing.Size(75, 23);
+            this.button_test.TabIndex = 0;
+            this.button_test.Text = "button_test";
+            this.button_test.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_test);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_test;
     }
 }
 
